95aee339beceb6602ec12aa1f2ea89174158ab6b
axelaction22 <axelaction22@aol.com>
2023-03-30 09:18:00 +0200

    Create template.php

---
+++ views/template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=<? $page_description ?>>
+    <title><? $page_title?></title>
+    <link rel="stylesheet" href="css.bootstrap.min.css">
+</head>
+    <body>
+        <? $page_content ?>
+    </body>
+</html>
