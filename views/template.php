6a7aea3fea809bf5b65b8d34577befbb96f9b3c1
axelaction22 <axelaction22@aol.com>
2023-03-31 13:36:19 +0200

    bs4 ->5 fix lol

--- views/template.php
+++ views/template.php
@@ -10,11 +10,13 @@
 </head>
 
     <body>
-    <script src="*\js\jquery-3.6.4.min.js"></script>
-    <script src="*\js\popper.min.js"></script>
-    <script src="*\js\bootstrap.bundle.min.js"></script>
         <?php require_once("menu.php");?>
         <?= $page_content ?>
+        
+
+    <script src="../js\jquery-3.6.4.min.js"></script>
+    <script src="../js\popper.min.js"></script>
+    <script src="../js\bootstrap.bundle.min.js"></script>
     </body>
 
 </html>
