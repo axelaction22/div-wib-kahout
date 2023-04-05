eb754152846ca98dfdb5187f8c43414a26c1f3df
axelaction22 <axelaction22@aol.com>
2023-04-05 08:53:01 +0200

    mvc structure

--- views/template.php
+++ views/template.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content=<?= $page_description ?>>
     <title><?= $page_title?></title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
 </head>
 
     <body>
@@ -15,9 +15,9 @@
         <?php require_once("inscription.html");?>
         
 
-    <script src="../js\jquery-3.6.4.min.js"></script>
-    <script src="../js\popper.min.js"></script>
-    <script src="../js\bootstrap.bundle.min.js"></script>
+    <script src="../assets\js\jquery-3.6.4.min.js"></script>
+    <script src="../assets\js\popper.min.js"></script>
+    <script src="../assets\js\bootstrap.bundle.min.js"></script>
     </body>
 
 </html>
