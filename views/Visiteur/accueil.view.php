6628ac6c8603eb42ef041eb2c35e32b37228cd14
NokX0 <119616486+NokX0@users.noreply.github.com>
2023-04-30 18:00:51 +0200

    Add files via upload

--- views/Visiteur/accueil.view.php
+++ views/Visiteur/accueil.view.php
@@ -1,3 +1,102 @@
-<h1>Accueil</h1>
+<link rel="stylesheet" href="public/CSS/accueil.css">
 
-cs
+
+
+<div id="logo">
+  <div class="cygle">
+
+    <div class="row">
+      <div class="dot blue"></div>
+      <div class="dot blue"></div>    
+      <div class="dot blue"></div>  
+    </div>
+
+    <div class="row">
+      <div class="dot green"></div>
+      <div class="dot green light"></div>    
+      <div class="dot green"></div>      
+    </div>
+
+      <div class="row">
+      <div class="dot blue"></div>
+      <div class="dot blue"></div>    
+      <div class="dot blue"></div>  
+    </div>
+  </div>
+
+  <h1>KAPOOT</h1>
+  <section class="pt-4">
+            <div class="container px-lg-5" >
+                <!-- Page Features-->
+                <div class="row gx-lg-5">
+                    <div class="col-lg-6 col-xxl-4 mb-5">
+                        <div class="p-4 p-lg-5 bg-black rounded-3 text-center">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                
+                                <h2 class="fs-4 fw-bold">Quiz N.1</h2>
+                                <p class="mb-0">Un quiz sur bla bla bla...</p>
+                                <br>
+                                <button type="button" class="btn btn-primary btn-lg">Un bouton</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xxl-4 mb-5">
+                        <div class="p-4 p-lg-5 bg-black rounded-3 text-center">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                
+                                <h2 class="fs-4 fw-bold">Quiz N.2</h2>
+                                <p class="mb-0">Un quiz sur bla bla bla...</p>
+                                <br>
+                                <button type="button" class="btn btn-primary btn-lg">Un autre bouton</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xxl-4 mb-5">
+                        <div class="p-4 p-lg-5 bg-black rounded-3 text-center">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                
+                                <h2 class="fs-4 fw-bold">Quiz N.3</h2>
+                                <p class="mb-0">Un quiz sur bla bla bla...</p>
+                                <br>
+                                <button type="button" class="btn btn-primary btn-lg">Pourquoi pas encore un autre bouton</button>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xxl-4 mb-5">
+                        <div class="p-4 p-lg-5 bg-black rounded-3 text-center">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                
+                                <h2 class="fs-4 fw-bold">Quiz N.4</h2>
+                                <p class="mb-0">Un quiz sur bla bla bla...</p>
+                                <br>
+                                <button type="button" class="btn btn-primary btn-lg">Le bouton après le bouton qui est après le bouton</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xxl-4 mb-5">
+                        <div class="p-4 p-lg-5 bg-black rounded-3 text-center">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                
+                                <h2 class="fs-4 fw-bold">Quiz N.5</h2>
+                                <p class="mb-0">Un quiz sur bla bla bla...</p>
+                                <br>
+                                <button type="button" class="btn btn-primary btn-lg">Le bouton qui suit le bouton après un autre bouton situé après un autre bouton</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xxl-4 mb-5">
+                        <div class="p-4 p-lg-5 bg-black rounded-3 text-center">
+                            <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                               
+                                <h2 class="fs-4 fw-bold">Quiz N.6</h2>
+                                <p class="mb-0">Un quiz sur bla bla bla...</p>
+                                <br>
+                                <button type="button" class="btn btn-primary btn-lg">La bouton qui suit tout les autres</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+</div>
--- views/Visiteur/login.view.php
+++ views/Visiteur/login.view.php
@@ -1,12 +1,42 @@
-<h1>Page de connexion</h1>
-<form method="POST" action="validation_login">
-  <div class="form-group">
-    <label for="login" class="form-label">Login</label>
-    <input type="text" class="form-control" id="login" name="login" placeholder="login">
-  </div>
-  <div class="form-group">
-    <label for="password" class="form-label">Password</label>
-    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-  </div>
-  <button type="submit" class="btn btn-primary">Connexion</button>
-</form>
+<link rel="stylesheet" href="public/CSS/Login.css">
+<div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-2"></div>
+            <div class="col-lg-6 col-md-8 login-box">
+                <div class="col-lg-12 login-key">
+                    <i class="fa fa-key" aria-hidden="true"></i>
+                </div>
+                <div class="col-lg-12 login-title">
+                    Login
+                </div>
+
+                <div class="col-lg-12 login-form">
+                    <div class="col-lg-12 login-form">
+                        <form>
+                            <div class="form-group">
+                                <label class="form-control-label">USERNAME</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label class="form-control-label">PASSWORD</label>
+                                <input type="password" class="form-control" i>
+                            </div>
+
+                            <div class="col-lg-12 loginbttm">
+                                <div class="col-lg-6 login-btm login-text">
+                                    <!-- Error Message -->
+                                </div>
+                                <div class="col-lg-6 login-btm login-button">
+                                    <button type="submit" class="btn btn-outline-primary">LOGIN</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-2"></div>
+            </div>
+        </div>
+
+
+
+
